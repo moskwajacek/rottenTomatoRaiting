d5ea4cd93fa51f8cd292150a1e13b02d08561108
moskwajacek <moskwajacek@gmail.com>
2021-08-25 22:11:31 +0200

    command arguments handling

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
 import os
+import argparse
 import requests
 from pathlib import Path
 from dotenv import load_dotenv
 
-
 env_path = Path('.') / '.env_local_development'
 load_dotenv(dotenv_path=env_path)
+parser = argparse.ArgumentParser()
 
-url = f"https://www.omdbapi.com/demo.aspx/?t=chicago&token={os.getenv('API_TOKEN')}"
-headers = {
-    'x-api-key': os.getenv('API_KEY')
-}
+parser.add_argument(
+    "--title", "-t", help="Set film title in quotes to see Rotten Tomatoes rating.")
+args = parser.parse_args()
 
-response = requests.request("GET", url, headers=headers).json()
+if args.title != None:
 
-for keyVal in response['Ratings']:
-    if (keyVal['Source'] == 'Rotten Tomatoes'):
-        print(keyVal['Value'])
+    api_key = os.getenv('API_KEY')
+    url = f"http://www.omdbapi.com?apikey={api_key}&t={args.title}"
+    response = requests.request("GET", url).json()
+
+    if len(response['Ratings']) < 0:
+        for keyVal in response['Ratings']:
+            if (keyVal['Source'] == 'Rotten Tomatoes'):
+                print("Rotten Tomatoes for film title is: %s" %
+                      keyVal['Value'])
+    else:
+        print('No raitings avealible for title.')
+else:
+    print("You should pass --title (-t) argument.")
